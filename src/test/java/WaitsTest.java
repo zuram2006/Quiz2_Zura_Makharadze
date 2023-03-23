81201196464744279d74fbcb9b5987d9825792b8
zuram2006 <zuram@bit.ge>
2023-03-23 15:22:44 +0400

    first installation

--- src/test/java/WaitsTest.java
+++ src/test/java/WaitsTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class WaitsTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class WaitsTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void open(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void WaitsTest1(){
+        driver.navigate().to("https://demoqa.com/progress-bar");
+        driver.findElement(By.id("startStopButton")).click();
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.attributeToBe(driver.findElement(By.xpath("//div[@role='progressbar']")),"aria-valuenow","50"));
+
+
+
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.quit();
+    }
+
+    //ZM
 }
--- src/test/java/WebFormsTest.java
+++ src/test/java/WebFormsTest.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class WebFormsTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ISelect;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions.*;
+
+import java.sql.Driver;
+import java.util.List;
+
+
+public class WebFormsTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void open(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void WebFormsTest1(){
+        driver.navigate().to("http://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+        Select dropdown = new Select(driver.findElement(By.id("dropdowm-menu-1")));
+        dropdown.selectByIndex(2);
+
+        WebElement Checkbox;
+        String RelativeXpath;
+
+        for (int i =1;i<5;i++){
+            RelativeXpath = "//input[@value='option-"+Integer.toString(i)+"']";
+            Checkbox = driver.findElement(By.xpath(RelativeXpath));
+
+            if (Checkbox.isSelected() == false){
+                driver.findElement(By.xpath(RelativeXpath)).click();
+            }
+        }
+
+        WebElement elem = driver.findElement(By.xpath("//option[@value='orange']"));
+        System.out.println("Orange Disabled Value is "+elem.getAttribute("disabled"));
+
+
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.quit();
+    }
+    //ZM
 }
